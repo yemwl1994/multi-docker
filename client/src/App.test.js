5862083f386957dcab23e105b366746dcccbdb0e
yemwl1994 <wlyem1223@gmail.com>
2018-11-12 10:54:36 +0800

    initial

---
+++ client/src/App.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {});
+// set up some dummy json in future for testing 
